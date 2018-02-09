f723abac26099f43ca86e86d1fd67b180ab111d6
naokikimura <n.kimura.cap@gmail.com>
2018-02-09 10:56:55 +0900

    Format api.js

--- app.js
+++ app.js
@@ -4,41 +4,41 @@ const debug = util.debuglog('jubaclient');
 const jubatus = require('jubatus');
 
 function toCamelCase(value) {
-  return value.replace(/_([a-z])/g, (match, group1) => group1.toUpperCase());
+    return value.replace(/_([a-z])/g, (match, group1) => group1.toUpperCase());
 }
 exports.toCamelCase = toCamelCase;
 
 function toSnakeCase(value) {
-  return value.replace(/[A-Z]/g, match => '_' + match.toLowerCase());
+    return value.replace(/[A-Z]/g, match => '_' + match.toLowerCase());
 }
 exports.toSnakeCase = toSnakeCase;
 
 function resolveService(service) {
-  const serviseName = toCamelCase('_' + service);
-  const { [serviseName.toLowerCase()]: { client: { [serviseName]: Service } } } = jubatus;
-  return Service;
+    const serviseName = toCamelCase('_' + service);
+    const { [serviseName.toLowerCase()]: { client: { [serviseName]: Service } } } = jubatus;
+    return Service;
 }
 exports.resolveService = resolveService;
 
 function request(service, method, params, rpcClient, name) {
-  const methodName = toCamelCase(method);
-  const Service = resolveService(service);
-  const client = new Service({ rpcClient, name });
-  debug(client);
-  return client[methodName].apply(client, params);
+    const methodName = toCamelCase(method);
+    const Service = resolveService(service);
+    const client = new Service({ rpcClient, name });
+    debug(client);
+    return client[methodName].apply(client, params);
 }
 exports.request = request;
 
 function assertServiceMethod(service, method) {
-  assert.ok(service && typeof service === 'string', 'service is required.');
-  assert.ok(method && typeof method === 'string', 'method is required.');
+    assert.ok(service && typeof service === 'string', 'service is required.');
+    assert.ok(method && typeof method === 'string', 'method is required.');
 
-  const serviseName = toCamelCase('_' + service);
-  const namespace = serviseName.toLowerCase();
-  assert.ok(Object.keys(jubatus).some(key => namespace === key), `${ namespace } is unspport service.`);
+    const serviseName = toCamelCase('_' + service);
+    const namespace = serviseName.toLowerCase();
+    assert.ok(Object.keys(jubatus).some(key => namespace === key), `${ namespace } is unspport service.`);
 
-  const { [namespace]: { client: { [serviseName]: Service } } } = jubatus;
-  const methodName = toCamelCase(method);
-  assert.ok(Object.keys(Service.prototype).some(key => methodName === key), `${ methodName } is unspport method.`);
+    const { [namespace]: { client: { [serviseName]: Service } } } = jubatus;
+    const methodName = toCamelCase(method);
+    assert.ok(Object.keys(Service.prototype).some(key => methodName === key), `${ methodName } is unspport method.`);
 }
 exports.assertServiceMethod = assertServiceMethod;
